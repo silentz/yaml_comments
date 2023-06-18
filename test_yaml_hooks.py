0a0ce9764950e831cff4e4d4cf410502a1510878
Max Pershin <mepershin@gmail.com>
2023-06-18 14:53:11 +0300

    broken tests and todos

--- test_yaml_hooks.py
+++ test_yaml_hooks.py
@@ -5,6 +5,23 @@ import yaml
 
 import yaml_hooks
 
+# TODO lists:
+#   * test before/after first list iter
+#   * test before/after last list iterm
+#   * test before/after inner list item
+#   * test on empty lists
+#   * test on single-item lists
+
+# TODO indents:
+#   * test different indents
+
+# TODO misc:
+#   * high depth of data structures
+#   * multiline strings in comments
+#   * comment before list vs comment before first list item
+#   * before/after list item inside a dict
+#   * before/after dict item inside a list
+
 
 class Tests:
     def dump_with_args(
@@ -45,5 +62,31 @@ b:
 c: null
 # before d
 d: true
+""".lstrip()
+        )
+
+    def test_after_global_keys(self) -> None:
+        data = {"a": "1", "b": [1, 1], "c": None, "d": True}
+        after = {
+            "^a$": "# after a",
+            "^b$": "# after b",
+            "^c$": "# after c",
+            "^d$": "# after d",
+        }
+        result = self.dump_with_args(data, after=after)
+        print(result)
+        assert (
+            result
+            == """
+a: '1'
+# after a
+b:
+- 1
+- 1
+# after b
+c: null
+# after c
+d: true
+# after d
 """.lstrip()
         )
